390be1d7fb73689865a7a357ea028cb4ecc8997d
yuma <yuma0518.basketball@gmail.com>
2020-11-24 02:12:28 +0900

    2-Q2 v2
    改良版

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,42 +1,49 @@
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(300, 300);
   let size = width / 8;
   noStroke();
   fill(128,128,128)
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      if(j % 2 >= 0.5 && i % 2 == 0){
-   rect(i * 20, j * 20, 20, 20);
- }
-   else if(j % 2 == 0 && i % 2 >= 0.5){
-   rect(i * 20, j * 20, 20, 20);
-                                      }
+      if(j % 2 == 1 && i % 2 == 0){
+   rect(i * size, j * size,size,size);
+                                  }
+   else if(j % 2 == 0 && i % 2 == 1){
+   rect(i * size, j * size, size, size);
+                                    }
                               }
                             }
- for(let x = 0; x < 8; x++){
-   for(let y = 0; y < 8; y++){
-    if(x % 2 == 0 && y % 2 == 1){
-      if(y >= 0 && y < 3){
-       fill(255, 0, 0);
-       ellipse(10 + 20 * x,10+ 20 * y,16);
-                         }
-       else if(y >= 5 && y < 8){
-         fill(0);
-       ellipse(10 + 20 * x,10+ 20 * y,16);
-                               }
-                               
-    }
-
-      if(x % 2 == 1 && y % 2 == 0){
-        if(y >= 0 && y < 3){
-        fill(255, 0, 0);
-       ellipse(10 + 20 * x,10+ 20 * y,16);
-                           }
-        else if(y >= 5 && y < 8){
-         fill(0);
-       ellipse(10 + 20 * x,10+ 20 * y,16);
-                                }
-                                  }
-                             }
+  for(let i = 0; i < 8; i++){
+   for(let j = 0; j < 8; j++){
+    if(i % 2 == 0){
+     if(j == 1){
+  fill(255,0,0);
+  ellipse(size * 1 / 2 + size * i, size * 1 / 2 + size * j, size * 4 / 5);
+               }
+    else if(j == 5){
+      fill(0)
+      ellipse(size * 1 / 2 + size * i, size * 1 / 2 + size * j, size * 4 / 5);
+                  }
+       else if(j == 7){
+      fill(0)
+      ellipse(size * 1 / 2 + size * i, size * 1 / 2 + size * j, size * 4 / 5);
+                      }
                     }
-          }
+     else{
+       if(j == 0){
+  fill(255,0,0);
+  ellipse(size * 1 / 2 + size * i, size * 1 / 2 + size * j, size * 4 / 5);
+                }
+    else if(j == 2){
+  fill(255,0,0);
+  ellipse(size * 1 / 2 + size * i, size * 1 / 2 + size * j, size * 4 / 5);
+                  }
+    else if(j == 6){
+      fill(0)
+      ellipse(size * 1 / 2 + size * i, size * 1 / 2 + size * j, size * 4 / 5);
+                      }
+     }
+                             }
+}
+
+}
