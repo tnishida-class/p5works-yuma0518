fea6d71f5093ac61536f63c7abe76dc88de03e3b
yuma <yuma0518.basketball@gmail.com>
2020-12-29 02:06:12 +0900

    4-q1
    4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,16 +1,25 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size= 50;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  if(count <= 50){
+    size = size + 1
+  }
+  else{
+    size = size - 1
+  }
+
+
   ellipse(width / 2, height / 2, size);
 }
